05c2ee746626b30486895ce772844d6567138d88
Sstark97 <aitorscinfo@gmail.com>
2022-05-25 20:57:35 +0100

    Add AGE_RATING CONSTANT

--- config/config.py
+++ config/config.py
@@ -1,8 +1,12 @@
 """Definicion del enlace a la Base de Datos."""
 DATA_BASE = "./config/geeks_hub.db"
+# Tablas
 SUSCRIPTION = "Suscripcion"
 FILM = "Pelicula"
 SERIES = "Serie"
 ACCOUNT = "Cuenta"
 PROFILE = "Perfil"
 HISTORY = "Historial"
+
+#Formularios
+AGE_RATING = [('', 'Selecciona un Genéro'), ('Tp', 'tp'), ('8', '8'), ('12', '12'), ('16', '16'), ('18', '18')]
