13b4a88138fe89e3a5dc24fed2b889c39834b92a
Javier Martel Espino <javiermespino@gmail.com>
2022-05-29 18:13:19 +0100

    Added Register routes

--- controllers/account_controller.py
+++ controllers/account_controller.py
@@ -1,7 +1,8 @@
 """Archivo de Rutas de las Cuentas."""
 import sys
-from bottle import get
+from bottle import route, run, template, request, get, post, redirect, static_file, error
 from models.account import Account
+from forms.register_form import RegistrationForm
 from config.config import DATA_BASE
 sys.path.append('models')
 
@@ -25,5 +26,33 @@ def account_index():
 
     # Select de todas las Cuentas
     row = cuentas.select(['*'])
-
     return str(row)
+
+@get('/register')
+def register():
+    """Pagina de inicio de Registro"""
+    form = RegistrationForm(request.POST)
+    return template('register', form=form)
+
+@post('/register')
+def register_process():
+    """Procesa el formulario de Registro de Cuentas"""
+    form = RegistrationForm(request.POST)
+    account = Account(DATA_BASE)
+    if form.register.data and form.validate():
+
+        form_data = {
+            "Correo" : form.email.data,
+            "Nombre" : form.name.data,
+            "Apellidos" : form.surname.data,
+            "Direccion" : form.direction.data,
+            "Contrasena" : form.password.data,
+            "Telefono" : form.phone_number.data,
+            "Tipo_Suscripcion" : form.suscription.data
+        }
+        # print(form_data)
+        account.insert(form_data)
+        redirect('/accounts')
+        
+    return template('register', form=form)
+    
