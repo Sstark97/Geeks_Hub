18864e9fc3aefdd706fbb5fd1ea916ab47bef84a
Javier Martel Espino <javiermespino@gmail.com>
2022-06-03 18:50:32 +0100

    Modified account settings controller

--- controllers/account_settings_controller.py
+++ controllers/account_settings_controller.py
@@ -3,41 +3,63 @@ import sys
 from bottle import get, request, template, redirect, post
 from models.account import Account
 from models.profile import Profile
-from config.config import DATA_BASE, ACCOUNT, PROFILE
+from forms.account_settings_form import AccountSettingsForm
+from config.config import DATA_BASE, ACCOUNT_FIELDS
 from config.local_storage import local_storage
 sys.path.append('models')
-# sys.path.append('forms')
+sys.path.append('forms')
 
 @get('/account_settings')
 def account_settings():
     """Página de inicio de Configuración de Cuentas"""
+    print(local_storage.getItem("email"))
+    print(local_storage.getItem("profile"))
+    form = AccountSettingsForm(request.POST)
     account = Account(DATA_BASE)
     profile = Profile(DATA_BASE)
     rows = account.select(['*'], {'Correo': local_storage.getItem("email")})
     rows_profile = profile.select(['*'], {'Correo': local_storage.getItem("email")})
-    return template('account_settings', rows=rows, rows_profile=rows_profile)
+
+    form.name.data = rows[0][1]
+    form.surname.data = rows[0][2]
+    form.direction.data = rows[0][3]
+    form.phone_number.data = rows[0][5]
+    form.suscription.data = rows[0][6]
+
+    return template('account_settings', rows=rows, rows_profile=rows_profile, form=form, fields=ACCOUNT_FIELDS)
 
 @post('/account_settings')
 def account_settings_process():
     """Procesa el formulario de Configuración de Cuentas"""
+    form = AccountSettingsForm(request.POST)
     account = Account(DATA_BASE)
     profile = Profile(DATA_BASE)
 
-    form_data_account = {
-        "Correo" : request.forms.get('email'),
-        "Nombre" : request.forms.get('name'),
-        "Apellidos" : request.forms.get('surname'),
-        "Direccion" : request.forms.get('direction'),
-        "Contrasena" : request.forms.get('password'),
-        "Telefono" : request.forms.get('phone_number'),
-        "Tipo_Suscripcion" : request.forms.get('suscription')
-    }
+    if form.submit.data:
+        form_data_account = {}
 
-    form_data_profile = {
-        "Nickname" : request.forms.get('nickname'),
-        "Imagen" : request.forms.get('avatar')
-    }
-
-    account.update(form_data_account, {'Correo': local_storage.getItem("email")})
-    profile.update(form_data_profile, {'Correo': local_storage.getItem("email")})
-    redirect('/account_settings')
+        if form.name.data:
+            form_data_account["Nombre"] = form.name.data
+        if form.surname.data:
+            form_data_account["Apellidos"] = form.surname.data
+        if form.direction.data:
+            form_data_account["Direccion"] = form.direction.data
+        if form.password.data:
+            form_data_account["Contrasena"] = form.password.data
+        if form.phone_number.data:
+            form_data_account["Telefono"] = form.phone_number.data
+        if form.suscription.data and form.suscription.data != '0':
+            form_data_account["Tipo_Suscripcion"] = form.suscription.data
+        
+        # form_data_profile = {
+        #     "Nickname" : request.forms.get('nickname'),
+        #     "Imagen" : request.forms.get('avatar')
+        # }
+        print(form_data_account)
+        account.update(form_data_account, {'Correo': local_storage.getItem("email")})
+        # profile.update(form_data_profile, {'Correo': local_storage.getItem("email")})
+        redirect('/account_settings')
+    
+    if form.logout.data:
+        local_storage.clear()
+        redirect('/')
