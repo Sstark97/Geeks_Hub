0d41047eafc44ab81883c305699b91cd0a3ff1ac
Sstark97 <aitorscinfo@gmail.com>
2022-06-05 18:16:15 +0100

    Finish basic email

--- utils/email.py
+++ utils/email.py
@@ -1,5 +1,8 @@
 """ Archivo para el manejo de correos electrónicos. """
-import smtplib, ssl
+import smtplib
+import ssl
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
 from os import getenv
 from dotenv import load_dotenv
 
@@ -12,3 +15,14 @@ def send_register_email(email):
 
     email_address = getenv('EMAIL')
     email_password = getenv('EMAIL_PASSWORD')
+
+    print(email_address)
+    print(email_password)
+
+    context = ssl.create_default_context()
+
+    with smtplib.SMTP_SSL(smtp_address, smtp_port, context=context) as server:
+        # Conexión con el servidor de correos.
+        server.login(email_address, email_password)
+        # Envío del correo.
+        server.sendmail(email_address, email, 'Registro de la Cuenta')
