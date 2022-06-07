d0bbbd711537ea35f1403606ce4f1151a6e40573
Sstark97 <aitorscinfo@gmail.com>
2022-06-07 19:38:49 +0100

    Complete RegisterForm Validator

--- forms/register_form.py
+++ forms/register_form.py
@@ -1,21 +1,27 @@
 """Fichero para el Formulario de Registro"""
-from wtforms import Form, StringField, PasswordField, SubmitField , validators
+from wtforms import Form, StringField, PasswordField, SubmitField, validators
+
 
 class RegistrationForm(Form):
     """Clase que maneja el formulario de Registro"""
-    
-    name = StringField('Nombre', [validators.InputRequired(), validators.Length(min=1, max=20)])
-    surname = StringField('Apellidos', [validators.InputRequired(), validators.Length(min=1, max=25)])
-    email = StringField('Correo electrónico', [validators.InputRequired(), validators.Length(min=6, max=60), validators.Email(message="Debe introducir un email válido")])
+
+    name = StringField(
+        'Nombre', [validators.InputRequired(), validators.Length(min=1, max=20)])
+    surname = StringField(
+        'Apellidos', [validators.InputRequired(), validators.Length(min=1, max=30)])
+    email = StringField('Correo electrónico', [validators.InputRequired(), validators.Length(min=6, max=60),
+                                               validators.Email(message="Debe introducir un email válido")])
+
     password = PasswordField('Contraseña', [
         validators.DataRequired(),
-        validators.EqualTo('password_confirm', 
+        validators.EqualTo('password_confirm',
                            message='Las contraseñas no coinciden')
-        ])
+    ])
     password_confirm = PasswordField('Repita la contraseña')
-    direction = StringField('Dirección', [validators.InputRequired(), validators.Length(min=1, max=70)])
-    phone_number = StringField('Teléfono', [validators.InputRequired(), validators.Length(min=9, max=9, message="Debe introducir un número de 9 dígitos")])  # 9 dígitos España
-    # birth_date = DateField('Fecha de Nacimiento', [validators.DataRequired()])    
-    
+    direction = StringField(
+        'Dirección', [validators.InputRequired(), validators.Length(min=1, max=70)])
+    phone_number = StringField('Teléfono', [validators.InputRequired(), validators.Length(min=9, max=9,
+                                                message="Debe introducir un número de 9 dígitos"),
+                                                validators.regexp('[0-9]+', message="El número debe contener solo digitos")]) 
+
     register = SubmitField('Registrar')
-    # cancel = SubmitField('Cancelar')
